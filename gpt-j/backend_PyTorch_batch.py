52296b2f6e222ecb549a30dede69575b78f3f571
bjmsong <bjmsong@126.com>
2024-07-08 15:09:02 +0800

    u

--- gpt-j/backend_PyTorch_batch.py
+++ gpt-j/backend_PyTorch_batch.py
@@ -14,7 +14,7 @@ gen_kwargs = {
     "early_stopping": True,
     "max_new_tokens": 128,
     "min_new_tokens": 30,
-    "num_beams": int(os.environ.get("GPTJ_BEAM_SIZE", "1")), # only beam_size 4 is allowed for official submission
+    "num_beams": int(os.environ.get("GPTJ_BEAM_SIZE", "4")), # only beam_size 4 is allowed for official submission
 }
 
 
@@ -28,7 +28,7 @@ class SUT_base():
         self.max_examples = max_examples
         self.scenario = scenario
         self.qsl = qsl
-        self.batch_size = 3
+        self.batch_size = 1
         print("Loading PyTorch model...")
             
         # dtype
