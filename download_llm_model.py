95bcbf9c91d3d5558490087343570d303544c999
bjmsong <bjmsong@126.com>
2024-07-17 16:10:25 +0800

    update

--- download_llm_model.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(repo_id='facebook/opt-125m',
-                  repo_type='model',
-                  local_dir='../model_dir',
-                  resume_download=True)
